aa2daa3c5bc09437bb7c75299c6cb7f1a55fe6ab
Aidan Welch <aidansw@yahoo.com>
2020-09-08 18:29:10 -0400

    updated camera parser

--- src/utils/CameraListParser.cpp
+++ src/utils/CameraListParser.cpp
@@ -12,17 +12,32 @@ CameraList CameraParse(std::string file_path) {
         std::string cur_county_name;
         std::string line;
         while(std::getline(file, line)){
+            bool moved_up = false;
+            bool escaped = false;
             bool in_string = false;
+            std::string key;
+            bool key_ended = false;
+            std::string value;
             for(uint i = 0; i < line.length(); i++){
-                in_string = (line[i] == '"') ^ in_string;
-                if(line[i] == '{' || line[i] == '['){
+                if(line[i] == '{' || line[i] == '[' && !in_string ){
                     nesting = static_cast<Nest>(nesting-1);
-                } else if (line[i] == '}' || line[i] == ']') {
+                } else if (line[i] == '}' || line[i] == ']' && !in_string ) {
+                    moved_up = true;
                     nesting = static_cast<Nest>(nesting+1);
-                } else {
-                    switch
+                } else if (line[i] == ':' && !in_string && !key_ended) {
+                    key_ended = true;
+                } else if (line[i] == '"' && !escaped) {
+                    in_string = !in_string;
+                } else if (line[i] != ' ' || in_string){
+                    if(!key_ended){
+                        key.push_back(line[i]);
+                    } else {
+                        value.push_back(line[i]);
+                    }
                 }
+                escaped = (line[i] == '\\' && !escaped);
             }
+
         }
         return cameras;
     } else {
