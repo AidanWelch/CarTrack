4ad21671c4467784c787dc54295a02a39d9b6e6f
Aidan Welch <aidan@freedwave.com>
2023-10-24 07:28:33 +0200

    Correct build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ find ./src/ -iname '*.h' -o -iname '*.cpp' | xargs clang-format-18 -style=file -
 mkdir build
 cd ./build
 cmake --build . --target clean
-cmake -G "Ninja" .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-# clang-tidy -p="build" --config-file="../.clang-tidy" ../src/ -extra-arg=-std=c++20
+cmake -G "Ninja" .. -D CMAKE_BUILD_TYPE=$1 -D CMAKE_EXPORT_COMPILE_COMMANDS=ON
 find ../src/ -iname '*.h' -o -iname '*.cpp' | xargs clang-tidy-18 -p="build" --config-file="../.clang-tidy" -extra-arg=-std=c++20
 ninja
